48fcb9ef874b1017778e29989a4f5d1f8a0c462b
Ishma <ishmakhan1995@gmail.com>
2024-09-01 23:46:13 +0530

    e-commerce layout

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+/*
+import About from './About'
+import Cart from './Cart'
+import Checkout from './Checkout'
+import Error from './Error'
+import HomeLayout from './HomeLayout'
+import Landing from './Landing'
+import Login from './Login'
+import Orders from './Orders'
+import Products from './Products'
+import Register from './Register'
+import SingleProduct from './SingleProduct'
+
+
+export {About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct}
+*/
+export { default as HomeLayout } from './HomeLayout';
+export { default as Landing } from './Landing';
+export { default as SingleProduct } from './SingleProduct';
+export { default as Products } from './Products';
+export { default as Cart } from './Cart';
+export { default as Error } from './Error';
+export { default as About } from './About';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as Checkout } from './Checkout';
+export { default as Orders } from './Orders';

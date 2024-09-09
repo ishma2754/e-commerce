e409e79fa5b2b4f3fb71aaa8e38f7e44f8c78994
Ishma <ishmakhan1995@gmail.com>
2024-09-09 23:45:11 +0530

    added filter functionality

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,6 +2,7 @@ import { Form, useLoaderData, Link } from "react-router-dom";
 import FormInput from "../components/FormInput";
 import FormSelect from "./FormSelect";
 import FormRange from "./FormRange";
+import FormCheckbox from "./FormCheckbox";
 
 const Filters = () => {
   const { meta } = useLoaderData();
@@ -36,7 +37,10 @@ const Filters = () => {
         size="select-sm"
       />
       {/**PRICE */}
-      <FormRange name='price' label='select price' size='range-sm'/>
+      <FormRange name='price' label='select price' 
+      size='range-sm'/>
+      {/**SHIPPING */}
+      <FormCheckbox name="shipping" label="free shipping" size="checkbox-sm"/>
       {/**BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         search
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,7 +8,15 @@ import { customFetch } from "../utils";
 const url = "/products";
 
 export const loader = async ({ request }) => {
-  const response = await customFetch(url);
+  //console.log(request)
+  //const params = new URL(request.url).searchParams
+  //const search = params.get('search')
+  //console.log(search)
+  const params = Object.fromEntries([...new URL(request.url).searchParams.entries()])
+  console.log(params)
+  const response = await customFetch(url, {
+    params
+  });
   const products = response.data.data;
   const meta = response.data.meta;
 
